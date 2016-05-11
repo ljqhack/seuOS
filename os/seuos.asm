3fa1eb3126b40ba17440d49a9f69e48a013ba275
ljqhack <ljqhack@qq.com>
2016-05-11 15:45:49 +0800

    pendsv

--- os/seuos.asm
+++ os/seuos.asm
@@ -12,13 +12,41 @@ OSStartHighRdy
 	STR R1, [R0]
 	CPSIE I
 
+
+OSCtxSw
+    LDR R0, =NVIC_INT_CTRL
+    LDR R1, =NVIC_PENDSVSET
+    STR R1, [R0]
+    BX LR
+
+
 OS_CPU_PendSVHandler
     CPSID I
     MRS R0,PSP
     CBZ R0,OSPendSV_nosave
 
-    SUB R0,R0, #0x20
+    SUBS R0,R0, #0x20
     STM R0, {R4-R11}
-    LDR R1, __OS_TCBCur
+    LDR R1, =OS_TCBCur
     LDR R1, [R1]
     STR R0, [R1]
+OSPendSV_nosave
+    LDR R0, =OSPrioCur
+    LDR R1, =OSPrioHighRdy
+    LDR R2,[R1]
+    STRB R2, [R0]
+    
+    LDR R0, =OSTCBCur
+    LDR R1, =OSTCBHighRdy
+    LDR R2, [R1]
+    STR R2, [R0]
+    
+    LDR R0, [R2]
+    LDM R0, {R4-R11}
+    ADDS R0, R0, #0x20
+    MSR PSP, R0
+    ORR LR, LR, #0x04
+    CPSIE I
+    BX LR
+
+    END   
